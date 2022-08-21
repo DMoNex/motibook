dc4825c857de1190c201d443e64a8f7d8e92f1f8
DMoNex <sonkho5231@gmail.com>
2022-08-21 16:29:28 +0900

    statistics number counter done note interface done

--- app/src/main/java/com/example/motibook/StatisticsFragment.java
+++ app/src/main/java/com/example/motibook/StatisticsFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,9 +22,10 @@ public class StatisticsFragment extends Fragment {
     private static final String TEXT_FOOTER = "권의 책을 읽었습니다.";
 
     // TODO: Rename and change types of parameters
-    private String header;
-    private String footer;
+    // private String header;
+    // private String footer;
     private int count; // 파일 입출력 통해 받을 값(읽은 책 수)
+    TextView countView;
 
     public StatisticsFragment() {
         // Required empty public constructor
@@ -33,8 +35,6 @@ public class StatisticsFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param
-     * @param
      * @return A new instance of fragment StatisticsFragment.
      */
     // TODO: Rename and change types and number of parameters
@@ -50,17 +50,21 @@ public class StatisticsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        count = 7777; // Test Line
         if (getArguments() != null) {
-            header = getArguments().getString(TEXT_HEADER);
-            footer = getArguments().getString(TEXT_FOOTER);
-            count = 1234; // TODO: 여기서 파일입출력으로 읽은 책의 수를 받아옴
+            count = 4896; // TODO: 여기서 파일입출력으로 읽은 책의 수를 받아옴
+            // header = getArguments().getString(TEXT_HEADER);
+            // footer = getArguments().getString(TEXT_FOOTER);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        // onCreate() 에서 받은 값들로 화면을 구성하는 함수
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_statistics, container, false);
+        countView = rootView.findViewById(R.id.textCountView);
+        countView.setText(Integer.toString(count));
+        return rootView;
     }
 }
