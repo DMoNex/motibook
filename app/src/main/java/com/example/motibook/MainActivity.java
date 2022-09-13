60904210755d13f14c281a000e6d39d9c6dbc084
DMoNex <sonkho5231@gmail.com>
2022-09-13 13:05:10 +0900

    Link Google ID

--- app/src/main/java/com/example/motibook/MainActivity.java
+++ app/src/main/java/com/example/motibook/MainActivity.java
@@ -1,25 +1,49 @@
 package com.example.motibook;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+
+
+    GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "MainActivity";
+    private final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +55,66 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        FragmentView(1); // 1: Statistics, 2: Note, 3: Subscribe
+        FragmentView(1); // 1: Statistics, 2: Note, 3: Subscribe. 4: Calender
+
+        /// Google Login API Start
+        // 최초 Default 로그인 객체 Build
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail() // email addresses도 요청함
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
+
+        // 로그인 이력 있으면 자동 로그인, 없으면 Email 요구
+        GoogleSignInAccount gsa = GoogleSignIn.getLastSignedInAccount(MainActivity.this);
+        if (gsa != null && gsa.getId() != null) {
+
+        }
+
+        // Login
+        signIn();
+        /// Google Login API End
     }
 
-    private void FragmentView(int fragmentNumber) {
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        activityResultLauncher.launch(signInIntent);
+    }
+
+    // Google Login 종료 시 실행
+    private ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+
+                // Login API 정상 종료
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getData() != null) {
+                        Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                        try {
+                            //Google Login Succeed
+                            GoogleSignInAccount account = task.getResult(ApiException.class);
+                            Log.d("LOGIN", "firebaseAuthWithGoogle:" + account.getId());
+
+                            // 사용자 이름 받아와 출력
+                            TextView nameView = (TextView) findViewById(R.id.namecard_name);
+                            nameView.setText(account.getDisplayName());
+
+                            // 사용자 Email 받아와 출력
+                            TextView emailView = (TextView) findViewById(R.id.namecard_mail);
+                            emailView.setText(account.getEmail());
+
+                            // 사용자 프로필 아이콘 받아와 출력
+                            ImageView imageView = (ImageView) findViewById(R.id.namecard_image);
+
+                        } catch (ApiException e) {
+
+                            //Google Login Failed
+                            Log.w("LOGIN", "Google sign in failed", e);
+                        }
+                    }
+                }
+            });
+
+
+    public void FragmentView(int fragmentNumber) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         switch(fragmentNumber) {
@@ -49,14 +129,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 transaction.commit();
                 break;
             case 3:
+                GoogleCalenderFragment calenderFragment = new GoogleCalenderFragment();
+                transaction.replace(R.id.Screen, calenderFragment);
+                transaction.commit();
                 break;
         }
     }
 
     @Override
     public void onBackPressed() {
+        Fragment subFragment = getSupportFragmentManager().findFragmentById(R.id.Screen);
         if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
             drawerLayout.closeDrawer(GravityCompat.END);
+        } else if (subFragment != null) {
+            Toast.makeText(this,"True", Toast.LENGTH_LONG);
+            ((OnBackPressedListener)subFragment).onBackPressed();
         } else {
             super.onBackPressed();
         }
@@ -70,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_menu_note) {
             FragmentView(2);
         } else if (id == R.id.nav_menu_event) {
-            //TODO
+            FragmentView(3);
         }
 
         drawerLayout.closeDrawer(navigationView);
@@ -82,4 +169,5 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction().replace(R.id.Screen, addBookFragment);
         transaction.commit();
     }
+
 }
