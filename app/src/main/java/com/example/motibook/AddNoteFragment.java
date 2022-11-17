3be0a056d6eae10b275822bf4ff667a09e6094b1
DMoNex <sonkho5231@gmail.com>
2022-11-17 21:51:08 +0900

    Rating Function Done

--- app/src/main/java/com/example/motibook/AddNoteFragment.java
+++ app/src/main/java/com/example/motibook/AddNoteFragment.java
@@ -6,12 +6,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -84,8 +88,42 @@ public class AddNoteFragment extends Fragment implements OnBackPressedListener {
         noteRating = (EditText)rootView.findViewById(R.id.ratingEditText);
 
         prevRating = Integer.parseInt(tmpArr[0].split("notes/")[1]);
+        newRating = prevRating;
         String strPrevRating = String.format("%.1f", prevRating / 10.0);
         noteRating.setText(strPrevRating);
+
+        noteRating.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!s.toString().isEmpty()) {
+                    double dbNewRating = Double.parseDouble(s.toString());
+                    if(dbNewRating < 0) {
+                        noteRating.setText("0.0");
+                        newRating = 0;
+                    }
+                    else if(dbNewRating > 5.0) {
+                        noteRating.setText("5.0");
+                        newRating = 50;
+                    }
+                    else {
+                        newRating = (int)(dbNewRating * 10);
+                    }
+                }
+                else {
+                    newRating = 0;
+                }
+            }
+        });
 
         // Filepath 이용하여 txt 파일 열기
         File txtFile = new File(mainAct.getLastFilePath());
@@ -110,11 +148,25 @@ public class AddNoteFragment extends Fragment implements OnBackPressedListener {
         MainActivity activity = (MainActivity) getActivity();
 
         try {
-            FileWriter fw = new FileWriter(mainAct.getLastFilePath());
+            String tmpArr[] = mainAct.getLastFilePath().split("#&#");
+
+            if(prevRating != newRating) {
+                String nTmpArr[] = tmpArr[0].split("notes/");
+                tmpArr[0] = nTmpArr[0] + "notes/" + newRating;
+            }
+
+            String newFileName = tmpArr[0] + "#&#" + tmpArr[1] + "#&#" + tmpArr[2];
+            FileWriter fw = new FileWriter(newFileName);
             String str = contents.getText().toString();
 
             fw.write(str);
             Toast.makeText(getActivity(), "저장되었습니다", Toast.LENGTH_SHORT).show();
+
+            if(prevRating != newRating) {
+                File prevFile = new File(mainAct.getLastFilePath());
+                prevFile.delete();
+                mainAct.setLastFilePath(newFileName);
+            }
 
             fw.flush();
             fw.close();
